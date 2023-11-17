84d6270598acca783c7c824f749179e13463a12d
sumitjambharkar <sujam3545@gmail.com>
2023-11-17 13:55:01 +0530

    first commit

--- server.js
+++ server.js
@@ -1,64 +1,16 @@
-const express = require("express");
-const multer = require("multer");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const path = require("path");
-const dotenv = require('dotenv')
+const express = require('express');
 const app = express();
+const IP = require('ip');
+const dotenv = require('dotenv')
 dotenv.config()
-const port = process.env.PORT;
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use("/image", express.static("image"));
+const Port = process.env.PORT 
+app.set('trust proxy', true);
 
-mongoose.connect(process.env.DATABASE, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+app.get('/getIpAddress', (req, res) => {
+    const ipAddress = IP.address();
+    res.send(ipAddress)
 });
 
-const imageSchema = new mongoose.Schema({
-  url: String,
-});
-
-const Image = mongoose.model("Image", imageSchema);
-
-let imageName = "";
-
-const storage = multer.diskStorage({
-  destination: path.join("./image"),
-  filename: function (req, file, cb) {
-    imageName = Date.now() + path.extname(file.originalname);
-    cb(null, imageName);
-  },
-});
-
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 3000000 },
-}).single("myImage");
-
-app.post("/upload-image", (req, res) => {
-  upload(req, res, async (err) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).json({ message: "Error uploading image" });
-    } else {
-      const imageUrl = `https://image-s6pl.onrender.com/image/${imageName}`;
-
-      try {
-        // Save the image URL to the database
-        await Image.create({ url: imageUrl });
-
-        return res.status(201).json({ message: "Image added", imageUrl });
-      } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "Error saving image to database" });
-      }
-    }
-  });
-});
-
-app.listen(port, () => {
-  console.log("Server running on port", port);
+app.listen(Port, () => {
+  console.log(`Server is running on http://localhost:${Port}`);
 });
